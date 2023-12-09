430f8abacba53d0138898f0c109f8f69f8f16a65
Kevin Ziegler <kevinziegler@Balder.asgard>
2023-12-09 12:27:57 -0600

    Fix advice function name in appearance config

--- conf.d/appearance.el
+++ conf.d/appearance.el
@@ -2,7 +2,7 @@
 (kdz/tab-bar-update-faces)
 
 ;; TODO Add this as a hook for catppuccin-reload
-(advice-add 'load-theme :after #'kdz/hl-todo-keyword-faces)
+(advice-add 'load-theme :after #'kdz/set-hl-todo-faces)
 (advice-add 'load-theme :after #'kdz/tab-bar-update-faces)
 
 (setq tab-bar-close-button-show nil)
