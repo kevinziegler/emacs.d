2ab8f38adfb6ad80d9582d685731d023d75e2572
Kevin Ziegler <kevinziegler@Balder.local>
2023-12-11 13:55:32 -0600

    Update buffer display behaviors

--- conf.d/behaviors.el
+++ conf.d/behaviors.el
@@ -1,6 +1,11 @@
 (setq switch-to-buffer-obey-display-actions t)
 (add-to-list 'display-buffer-alist
 	     '("\\*Messages\\*" display-buffer-in-new-tab nil))
-(add-to-list '("\\*ielm\\*" display-buffer-in-side-window '((side . bottom)
+(add-to-list 'display-buffer-alist
+	     '("\\*Ibuffer\\*" display-buffer-in-new-tab nil))
+(add-to-list 'display-buffer-alist
+	     '("\\*Warnings\\*" display-buffer-in-new-tab nil))
+(add-to-list 'display-buffer-alist
+	     '("\\*ielm\\*" display-buffer-in-side-window '((side . bottom)
 							    (slot . 99)
 							    (dedicated . t))))
