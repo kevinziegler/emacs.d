7c3bea650da8fa068426cc688dc25bb4adf22bf3
Kevin Ziegler <kevinziegler@Balder.local>
2023-12-13 12:14:05 -0600

    Fix up window parameters for buffer display rules

--- conf.d/behaviors.el
+++ conf.d/behaviors.el
@@ -1,11 +1,30 @@
 (setq switch-to-buffer-obey-display-actions t)
+
 (add-to-list 'display-buffer-alist
-	     '("\\*Messages\\*" display-buffer-in-new-tab nil))
+	     '("\\*ielm\\*"
+	       display-buffer-in-side-window
+	       (side . bottom)
+	       (slot . 99)
+	       (dedicated . t)))
+
 (add-to-list 'display-buffer-alist
-	     '("\\*Ibuffer\\*" display-buffer-in-new-tab nil))
+	     '("\\*helpful [a-z]+: .+\\*"
+	       display-buffer-in-side-window
+	       (side . right)
+	       (dedicated . t)))
+
 (add-to-list 'display-buffer-alist
-	     '("\\*Warnings\\*" display-buffer-in-new-tab nil))
+	     '("\\*Ibuffer\\*"
+	       display-buffer-in-tab
+	       (tab-name . "Buffers")))
+
 (add-to-list 'display-buffer-alist
-	     '("\\*ielm\\*" display-buffer-in-side-window '((side . bottom)
-							    (slot . 99)
-							    (dedicated . t))))
+	     '("\\*Messages\\*"
+	       display-buffer-in-tab
+	       (tab-name . "System")))
+
+(add-to-list 'display-buffer-alist
+	     '("\\*Warnings\\*"
+	       display-buffer-in-tab
+	       (tab-name . "System")))
+
