67ffecef26bcbd2d2b2247d0fa0ef6e73c8da6da
Kevin Ziegler <kevinziegler@Balder.local>
2023-12-13 12:14:55 -0600

    Adjust which-key sorting order

--- packages.d/utilities.el
+++ packages.d/utilities.el
@@ -174,6 +174,7 @@
   :init
   (setq which-key-sort-order 'which-key-prefix-then-key-order
 	which-key-max-display-columns 5
+	which-key-sort-uppercase-first nil
 	which-key-prefix-prefix "☰ ")
   (which-key-mode))
 
