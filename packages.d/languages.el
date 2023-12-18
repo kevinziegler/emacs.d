847036a2fd5aab953202b592170212b352d1049b
Kevin Ziegler <kevinziegler@Balder.asgard>
2023-12-18 10:49:04 -0600

    Fix up mode specifications for language packages

--- packages.d/languages.el
+++ packages.d/languages.el
@@ -1,17 +1,17 @@
-(use-package dotenv-mode :straight t :mode '("\\.env\\..*\\'" . dotenv-mode))
+(use-package dotenv-mode :straight t :mode (("\\.env\\..*\\'" . dotenv-mode)))
 
-(use-package kotlin-ts-mode :straight t :mode '("\\.kt\\'" . kotlin-ts-mode))
+(use-package kotlin-ts-mode :straight t :mode (("\\.kt\\'" . kotlin-ts-mode)))
 
 (use-package lua-mode
   :straight t
-  :mode '("\\.lua\\'" . lua-mode)
+  :mode (("\\.lua\\'" . lua-mode))
   :interpreter'("lua" . lua-mode)
   :config
   (autoload 'lua-mode "lua-mode" "Lua editing mode." t))
 
-(use-package markdown-mode :straight t :mode '("\\.md\\'" . markdown-mode))
+(use-package markdown-mode :straight t :mode (("\\.md\\'" . markdown-mode)))
 
-(use-package php-mode :straight t :mode '("\\.php\\'" . php-mode))
+(use-package php-mode :straight t :mode (("\\.php\\'" . php-mode)))
 
 (use-package plantuml-mode
   :straight t
@@ -20,9 +20,9 @@
   :config
   (setq plantuml-default-exec-mode 'executable))
 
-(use-package scad-mode :straight t :mode '("\\.scad\\'" . scad-mode))
+(use-package scad-mode :straight t :mode (("\\.scad\\'" . scad-mode)))
 
-(use-package toml-mode :straight t :mode '("\\.toml\\'" . toml-mode))
+(use-package toml-mode :straight t :mode (("\\.toml\\'" . toml-mode)))
 
 (use-package web-mode
   :mode (("\\.html?\\'" . web-mode)
@@ -36,6 +36,6 @@
   (setq web-mode-code-indent-offset 2)
   (setq web-mode-content-types-alist '(("jsx" . "\\.js[x]?\\'"))))
 
-(use-package yaml-mode :straight t :mode '("\\.ya?ml\\'" . yaml-mode))
+(use-package yaml-mode :straight t :mode (("\\.ya?ml\\'" . yaml-mode)))
 
 
