27de6e08c514acdcbc7e9b497fa7a9fa54712844
Kevin Ziegler <kevinziegler@Balder.local>
2023-12-21 11:13:11 -0600

    Add `exec-path-from-shell`

--- packages.d/utilities.el
+++ packages.d/utilities.el
@@ -260,6 +260,11 @@
 
 (use-package list-environment
   :straight t)
+(use-package exec-path-from-shell
+  :config
+  (when (memq window-system '(mac ns x))
+    (exec-path-from-shell-initialize)))
+
 
 (use-package undo-fu
   :straight t
