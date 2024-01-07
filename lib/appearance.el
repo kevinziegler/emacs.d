3d17041c980776742a5f5f951dc932777bd77b15
Kevin Ziegler <kevinziegler@Balder.asgard>
2024-01-07 13:02:27 -0600

    Experiment with showing icons in tab-bar SVG images
    Getting the SVG icon to show up works fine (at least for `material`
    icons; `boxicons` at least seems to have issues), but getting the
    rendered SVG to abide by the `:crop-left` option doesn't appear to
    work.  I'm not sure if this is an issue with the padding argument
    being passed, or if it's something deeper with `svg-lib` itself.

--- lib/appearance.el
+++ lib/appearance.el
@@ -19,7 +19,7 @@
 
 (defun eli/tab-bar-svg-padding (width string)
   (let* ((style svg-lib-style-default)
-         (margin      (plist-get style :margin))
+         (margin (plist-get style :margin))
          (txt-char-width  (window-font-width nil 'fixed-pitch))
          (tag-width (- width (* margin txt-char-width)))
          (padding (- (/ tag-width txt-char-width) (length string))))
@@ -45,19 +45,21 @@
                                   (- padding number-padding)
                                   :crop-left))))
 
-(defun kdz/tab-bar-tab-svg-segment
-    (text inverse padding crop)
-  (propertize
-   text
-   'display
-   (svg-tag-make text
-                 crop t
-                 :font-size (* 12 kdz-tab-bar-tab-svg-scale)
-                 :height kdz-tab-bar-tab-svg-scale
-                 :inverse inverse
-                 :margin kdz-tab-bar-tab-svg-margin
-                 :padding padding
-                 :radius kdz-tab-bar-tab-svg-radius)))
+(defun kdz/tab-bar-tab-svg-segment (text inverse padding crop &optional icon)
+  (let ((foreground-attr (if inverse :background :foreground))
+        (background-attr (if inverse :foreground :background)))
+    (propertize
+     text
+     'display
+     (svg-tag-make (concat icon text)
+                   crop t
+                   :font-size (* 12 kdz-tab-bar-tab-svg-scale)
+                   :font-family (ns-font-name (frame-parameter nil 'font))
+                   :height kdz-tab-bar-tab-svg-scale
+                   :inverse inverse
+                   :margin kdz-tab-bar-tab-svg-margin
+                   :padding (if icon (- padding (window-font-width nil 'fixed-pitch)) padding)
+                   :radius kdz-tab-bar-tab-svg-radius))))
 
 (defun kdz/set-hl-todo-faces (&rest _)
   "Set face colors for hl-todo keywords"
@@ -88,7 +90,7 @@
   (let ((current-p (eq (car tab) 'current-tab)))
     (propertize
      (concat " "
-	     (if tab-bar-tab-hints (format ":%d: " i) "")
+	     (if tab-bar-tab-hints (format "[%d] " i) "")
              (alist-get 'name tab)
              (or (and tab-bar-close-button-show
                       (not (eq tab-bar-close-button-show
@@ -104,3 +106,73 @@
               (propertize "» " 'face 'vertico-current)
             "  ")
           cand))
+
+(defun kdz/tab-bar-format-toggle ()
+  (interactive)
+  (setq tab-bar-tab-name-format-function
+        (if (eq tab-bar-tab-name-format-function
+                'kdz/tab-bar-tab-name-format-svg)
+            'kdz/tab-bar-tab-name-format-text
+          'kdz/tab-bar-tab-name-format-svg) ))
+
+(defun kdz/tab-bar-left-elements-size ()
+  (let* ((elements (seq-take-while
+                    (lambda (item)
+                      (not (eq item 'kdz/tab-bar-format-tabs-centered)))
+                    tab-bar-format))
+         (expanded (seq-map #'funcall elements))
+         (sizes (seq-map #'length expanded)))
+    (seq-reduce #'+ sizes 0)))
+
+(defun kdz/tab-bar-right-elements-size ()
+  (let* ((elements (cdr (seq-drop-while
+                         (lambda (item)
+                           (not (eq item 'kdz/tab-bar-format-tabs-centered)))
+                         tab-bar-format) ))
+         (expanded (seq-map #'funcall elements))
+         (sizes (seq-map #'length expanded)))
+    (seq-reduce #'+ sizes 0)))
+
+(defun kdz/tab-bar-list-centering-width ()
+  (let* ( (tab-width (kdz/tab-bar-tab-width))
+          (tab-list-tabs-width (* (length (tab-bar-tabs) tab-width)))
+          (tab-list))))
+
+(defun kdz/tab-bar-spacer (side width-in-chars)
+  (list (intern (format "sep-%s" side))
+        'menu-item
+        (make-string width-in-chars ?\ )
+        'ignore))
+
+(defun kdz/tab-bar-format-tabs-centered ()
+  (let* ((tab-width (kdz/tab-bar-tab-width))
+         (tab-list-tabs-width (* (length (tab-bar-tabs)) tab-width))
+         (tab-list (funcall tab-bar-tabs-function))
+         (tab-separators (- (length tab-list) 1))
+         (tab-separator-length (length (tab-bar-separator)))
+         (txt-char-width (window-font-width nil 'fixed-pitch))
+         (tab-list-px (+ (* (length tab-list) tab-width)
+                         (* tab-separators
+                            (length (tab-bar-separator))
+                            txt-char-width)))
+         (centering-px-per-side (/ (- (frame-inner-width) tab-list-px)
+                                   2))
+         (centering-chars-per-side (/ centering-px-per-side
+                                      txt-char-width))
+         (left-content-length (kdz/tab-bar-left-elements-size))
+         (right-content-length (kdz/tab-bar-right-elements-size))
+         (left-padding (- centering-chars-per-side left-content-length 1))
+         (right-padding (- centering-chars-per-side right-content-length)))
+    (append (list (kdz/tab-bar-spacer "left" left-padding))
+            (let ((i 0))
+              (mapcan
+               (lambda (tab)
+                 (setq i (1+ i))
+                 (tab-bar--format-tab tab i))
+               tab-list))
+            (list (kdz/tab-bar-spacer "right" right-padding))
+            )
+    ))
+
+(setq tab-bar-format '(tab-bar-format-history tab-bar-format-tabs tab-bar-separator))
+(setq tab-bar-format '(tab-bar-format-history kdz/tab-bar-format-tabs-centered tab-bar-separator))
