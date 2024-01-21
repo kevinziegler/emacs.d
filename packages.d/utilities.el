091927f793b135625b5787e31f8d2730a35e9125
Kevin Ziegler <kevinziegler@Balder.asgard>
2024-01-21 13:17:09 -0600

    Re-organize 'utilities' packages

--- packages.d/utilities.el
+++ packages.d/utilities.el
@@ -1,7 +1,95 @@
-;; UI-related elements, themes, etc.
-(use-package catppuccin-theme :straight t)
 
+;;; Core Emacs Configuration
+(use-package emacs
+  :init
+  (global-set-key (kbd "<escape>") 'keyboard-escape-quit)
+  (set-charset-priority 'unicode)
+
+  (setq locale-coding-system 'utf-8
+        coding-system-for-read 'utf-8
+        coding-system-for-write 'utf-8)
+
+  (set-terminal-coding-system 'utf-8)
+  (set-keyboard-coding-system 'utf-8)
+  (set-selection-coding-system 'utf-8)
+  (prefer-coding-system 'utf-8)
+
+  (setq default-process-coding-system '(utf-8-unix . utf-8-unix))
+
+  ;; Emacs 28: Hide commands in M-x which do not apply to the current
+  ;;           mode.  Corfu commands are hidden, since they are not
+  ;;           supposed to be used via M-x.
+  (setq read-extended-command-predicate #'command-completion-default-include-p
+        tab-always-indent 'complete)
+
+  ;; Use `y' and `n' prompts instead of `yes' and `no'
+  (defalias 'yes-or-no-p 'y-or-n-p))
+
+;;;; Instance-level behaviors
+(use-package list-environment :straight t)
+(use-package exec-path-from-shell
+  :config
+  (when (memq window-system '(mac ns x))
+    (exec-path-from-shell-initialize)))
+
+;;;; Libraries
+(use-package f :straight t)
+
+;;; UI improvements and extras
+;;;; Foundational UI Packages
+(use-package hydra
+  :straight t
+  :config
+  (setq hydra-hint-display-type 'posframe)
+  (setq hydra-posframe-show-params
+        `(:poshandler posframe-poshandler-frame-center
+                      :internal-border-width 2
+                      :internal-border-color "#61AFEF"
+                      :left-fringe 16
+                      :right-fringe 16)))
+
+(use-package sideline
+  :straight t
+  :init
+  (setq sideline-backends-right '((sideline-blame . down)))
+  :config
+  (global-sideline-mode 1))
+
+(use-package nerd-icons :straight t)
+
+;;;; Cosmetic extras, themes, etc.
+(use-package catppuccin-theme :straight t)
+(use-package creamsody-theme :straight t)
+
+(use-package spacious-padding
+  :straight t
+  :config
+  (setq spacious-padding-widths (list :internal-border-width 7
+                                      :header-line-width 4
+                                      :mode-line-width 6
+                                      :tab-width 4
+                                      :right-divider-width 7
+                                      :scroll-bar-width 0))
+  (spacious-padding-mode))
+
+(use-package solaire-mode
+  :straight t
+  :after catppuccin-theme
+  :config
+  (solaire-global-mode +1))
+
+(use-package svg-tag-mode :straight t :config (global-svg-tag-mode))
+
+(use-package nerd-icons-ibuffer
+  :straight t
+  :hook (ibuffer-mode . nerd-icons-ibuffer-mode)
+  :config
+  (setq nerd-icons-ibuffer-icon t))
+
+;;;; Dashboard-related packages
 (use-package dashboard
+  :straight t
+  :after nerd-icons
   :init
   (setq dashboard-icon-type 'nerd-icons)
   :config
@@ -12,14 +100,21 @@
         dashboard-projects-backend 'project-el
         dashboard-projects-switch-function
 	#'tabspaces-open-or-create-project-and-workspace)
+
   (dashboard-setup-startup-hook))
 
-(use-package dashboard-project-status
+(use-package dashboard-ls
   :after dashboard
-  :straight t)
+  :straight t
+  :config
+  (add-to-list 'dashboard-heading-icons
+               '(ls-directories . "nf-oct-file_directory"))
+  (add-to-list 'dashboard-heading-icons
+               '(ls-files . "nf-oct-file")))
 
-(use-package dashboard-ls :straight t)
+(use-package dashboard-project-status :after dashboard :straight t)
 
+;;;; Use Doom's Modeline
 (use-package doom-modeline
   :straight t
   :init (doom-modeline-mode 1)
@@ -29,29 +124,15 @@
 	doom-modeline-persp-name nil
 	doom-modeline-buffer-encoding nil))
 
-(use-package spacious-padding
-  :straight t
+;;;; Workspace/project management
+(use-package project
   :config
-  (setq spacious-padding-widths
-	'(:internal-border-width 7
-                                 :header-line-width 4
-                                 :mode-line-width 6
-                                 :tab-width 4
-                                 :right-divider-width 7
-                                 :scroll-bar-width 0))
-  (spacious-padding-mode))
-
-(use-package solaire-mode
-  :straight t
-  :after 'catppuccin-theme
-  :config
-  (solaire-global-mode +1))
-
-;; TODO Configure for org-mode
-(use-package svg-tag-mode
-  :straight t
-  :config
-  (global-svg-tag-mode))
+  (advice-add 'project-switch-project
+              :before
+              #'kdz/project-open-show-dashboard)
+  (advice-add 'project-switch-project
+              :after
+              #'kdz/project-kill-dashboard))
 
 (use-package tab-bar
   :straight nil
@@ -62,9 +143,8 @@
   ;; (advice-add tab-bar-tabs-function
   ;;             :filter-return
   ;;             #'kdz/tab-bar-tabs-sort-pinned-tabs-last)
-
-  (delete 'tab-bar-format-add-tab tab-bar-format)
-  (setq tab-bar-format '(tab-bar-separator
+  (setq tab-bar-new-tab-to 'rightmost
+        tab-bar-format '(tab-bar-separator
                          kdz/tab-bar-format-project-icon
                          tab-bar-separator
                          kdz/tab-bar-format-unpinned-tabs
@@ -94,6 +174,21 @@
   (tab-bar-select-tab-by-name "Home")
   (tab-bar-close-tab-by-name "*scratch*"))
 
+(use-package ibuffer-project
+  :straight t
+  :config
+  (add-hook 'ibuffer-hook
+	    (lambda ()
+	      (setq ibuffer-filter-groups
+                    (ibuffer-project-generate-filter-groups))
+	      (unless (eq ibuffer-sorting-mode 'project-file-relative)
+		(ibuffer-do-sort-by-project-file-relative))))
+  (add-hook 'ibuffer-hook
+	    (lambda ()
+	      (setq ibuffer-filter-groups
+                    (ibuffer-project-generate-filter-groups)))))
+
+;;;; Treemacs as a sidebar explorer
 (use-package treemacs
   :straight t
   :defer t
@@ -110,37 +205,16 @@
 
 (use-package treemacs-tab-bar
   :straight t
-  :after (treemacs)
+  :after treemacs
   :config (treemacs-set-scope-type 'Tabs))
 
-(use-package treemacs-evil
-  :straight t
-  :after (treemacs evil))
+(use-package treemacs-evil :straight t :after (treemacs evil))
 
-;; Minibuffer/completion utlities
-(use-package cape
-  :straight t
-  :init
-  (add-to-list 'completion-at-point-functions #'cape-file))
-
+;;;; Minibuffer Completion (Consult, Vertico, etc)
 (use-package consult
   :straight t
   :config
   (setq consult-narrow-key "<"))
-
-(use-package corfu
-  :straight t
-  :custom
-  (corfu-auto t)
-  :init
-  (global-corfu-mode)
-
-  ;; https://github.com/emacs-evil/evil-collection/issues/766
-  (advice-remove 'corfu--setup 'evil-normalize-keymaps)
-  (advice-remove 'corfu--teardown 'evil-normalize-keymaps)
-
-  (advice-add 'corfu--setup :after (lambda (&rest r) (evil-normalize-keymaps)))
-  (advice-add 'corfu--teardown :after  (lambda (&rest r) (evil-normalize-keymaps))))
 
 (use-package embark
   :straight t
@@ -150,7 +224,6 @@
    ("C-h B" . embark-bindings)) ;; alternative for `describe-bindings'
 
   :init
-
   ;; Optionally replace the key help with a completing-read interface
   (setq prefix-help-command #'embark-prefix-help-command)
 
@@ -170,15 +243,7 @@
   :straight t
   :hook ((embark-collect-mode . consult-preview-at-point-mode)))
 
-(use-package marginalia
-  :straight t
-  :init (marginalia-mode))
-
-(use-package nerd-icons-corfu
-  :straight t
-  :after corfu
-  :config
-  (add-to-list 'corfu-margin-formatters #'nerd-icons-corfu-formatter))
+(use-package marginalia :straight t :init (marginalia-mode))
 
 (use-package orderless
   :straight t
@@ -204,20 +269,47 @@
   ;; Tidy shadowed file names
   :hook ((rfn-eshadow-update-overlay . vertico-directory-tidy)))
 
-(use-package posframe :straight t)
-
 (use-package vertico-posframe
   :straight t
+  :after '(posframe vertico)
   :config
   (vertico-posframe-mode 1)
   (setq vertico-posframe-poshandler #'kdz/posframe-offset-top)
   (setq vertico-posframe-parameters '((left-fringe . 8)
                                       (right-fringe . 8))))
 
-;; Miscellaneous
-(use-package helpful :straight t)
+;;;; Completion-at-point (Cape/Corfu)
+(use-package cape
+  :straight t
+  :config
+  (add-to-list 'completion-at-point-functions #'cape-file))
 
+(use-package corfu
+  :straight t
+  :custom
+  (corfu-auto t)
+  :config
+  (global-corfu-mode)
+
+  ;; https://github.com/emacs-evil/evil-collection/issues/766
+  (advice-remove 'corfu--setup 'evil-normalize-keymaps)
+  (advice-remove 'corfu--teardown 'evil-normalize-keymaps)
+
+  (advice-add 'corfu--setup
+              :after (lambda (&rest r) (evil-normalize-keymaps)))
+  (advice-add 'corfu--teardown
+              :after (lambda (&rest r) (evil-normalize-keymaps))))
+
+(use-package nerd-icons-corfu
+  :straight t
+  :after corfu
+  :config
+  (add-to-list 'corfu-margin-formatters #'nerd-icons-corfu-formatter))
+
+;;; Help/Discoverability
+(use-package helpful :straight t)
 (use-package general :straight t)
+(use-package posframe :straight t)
 
 (use-package which-key
   :straight t
@@ -238,29 +330,29 @@
               #'kdz/fixup--which-key-posframe--show-buffer)
   (which-key-posframe-mode 1))
 
-(use-package emacs
-  :init
-  (global-set-key (kbd "<escape>") 'keyboard-escape-quit)
-  (set-charset-priority 'unicode)
 
-  (setq locale-coding-system 'utf-8
-        coding-system-for-read 'utf-8
-        coding-system-for-write 'utf-8)
+;;;; In-Buffer UI Enhancments - Editing behaviors, formating, etc
+(use-package hl-todo :straight t :config (global-hl-todo-mode))
+(use-package browse-at-remote :straight t) ;; TODO Set up keybindings
+(use-package perfect-margin :straight t)
+(use-package imenu-list :straight t)
+(use-package yasnippet :straight t :config (yas-global-mode))
+(use-package yasnippet-snippets :straight t :after 'yasnippet)
+(use-package consult-yasnippet :straight t :after 'yasnippet)
+(use-package copy-as-format :straight t)
+(use-package separedit :straight t)
+(use-package expand-region :straight t)
+(use-package jinx :straight t :config (global-jinx-mode))
+(use-package apheleia :straight t :config (apheleia-global-mode +1))
+(use-package vundo :straight t)
+(use-package eval-sexp-fu :straight t)
+(use-package anzu :straight t :config (global-anzu-mode +1))
+(use-package sideline-blame :straight t)
+(use-package sideline-lsp :straight t)
 
-  (set-terminal-coding-system 'utf-8)
-  (set-keyboard-coding-system 'utf-8)
-  (set-selection-coding-system 'utf-8)
-  (prefer-coding-system 'utf-8)
-
-  (setq default-process-coding-system '(utf-8-unix . utf-8-unix))
-
-  ;; Emacs 28: Hide commands in M-x which do not apply to the current mode.
-  ;; Corfu commands are hidden, since they are not supposed to be used via M-x.
-  (setq read-extended-command-predicate #'command-completion-default-include-p
-        tab-always-indent 'complete)
-
-  ;; Use `y' and `n' prompts instead of `yes' and `no'
-  (defalias 'yes-or-no-p 'y-or-n-p))
+(use-package hide-mode-line
+  :straight t
+  :hook ((reb-mode . hide-mode-line-mode)))
 
 (use-package elec-pair
   :ensure nil
@@ -272,27 +364,11 @@
   :config
   (global-git-gutter-mode 1))
 
-(use-package git-modes
-  :straight t
-  :config
-  (add-to-list 'auto-mode-alist (cons "/.dockerignore\\'" 'gitignore-mode)))
-
-(use-package hl-todo
-  :straight t
-  :config
-  (global-hl-todo-mode))
-
-;; TODO Set up keybindings
-(use-package browse-at-remote
-  :straight t)
-
 (use-package modern-fringes
   :config
   (fringe-mode)
   (modern-fringes-mode)
   (modern-fringes-invert-arrows))
-
-(use-package f :straight t)
 
 (use-package origami
   :straight t
@@ -300,111 +376,34 @@
   (setq origami-fold-replacement " ... ")
   (global-origami-mode))
 
-(use-package magit
-  :straight t)
-
-(use-package magit-delta
-  :straight t
-  :hook ((magit-mode . magit-delta-mode)))
-
-(use-package git-timemachine
-  :straight t)
-
-(use-package f
-  :straight t)
-
 (use-package repl-toggle
   :straight t
   :config
-  (setq rtog/fullscreen t
+  (setq rtog/fullscreen nil
+        rtog/fallback-repl-fun #'ielm
 	rtog/mode-repl-alist '((emacs-lisp-mode . ielm)
 			       (python-mode . run-python)
 			       (python-ts-mode . run-python))))
 
-(use-package ibuffer-project
+(use-package ws-butler
+  :straight t
+  :hook ((prog-mode . ws-butler-mode)))
+
+(use-package editorconfig
   :straight t
   :config
-  (add-hook 'ibuffer-hook
-	    (lambda ()
-	      (setq ibuffer-filter-groups (ibuffer-project-generate-filter-groups))
-	      (unless (eq ibuffer-sorting-mode 'project-file-relative)
-		(ibuffer-do-sort-by-project-file-relative))))
-  (add-hook 'ibuffer-hook
-	    (lambda ()
-	      (setq ibuffer-filter-groups (ibuffer-project-generate-filter-groups)))))
-
-(use-package nerd-icons-ibuffer
-  :straight t
-  :hook (ibuffer-mode . nerd-icons-ibuffer-mode)
-  :config
-  (setq nerd-icons-ibuffer-icon t))
-
-(use-package list-environment
-  :straight t)
-
-(use-package exec-path-from-shell
-  :config
-  (when (memq window-system '(mac ns x))
-    (exec-path-from-shell-initialize)))
+  (setopt editorconfig-trim-whitespaces-mode 'ws-butler-mode)
+  (editorconfig-mode 1))
 
 (use-package undo-fu
   :straight t
   :config
   (setopt evil-undo-system 'undo-fu))
 
-(use-package vundo :straight t)
-
-(use-package perfect-margin :straight t)
-
-(use-package imenu-list :straight t)
-
 (use-package file-info
   :straight t
   :config
   (setq file-info-headline-underline-symbol ?━))
-
-(use-package hydra
-  :straight t
-  :config
-  (setq hydra-hint-display-type 'posframe)
-  (setq hydra-posframe-show-params
-        `(:poshandler posframe-poshandler-frame-center
-                      :internal-border-width 2
-                      :internal-border-color "#61AFEF"
-                      :left-fringe 16
-                      :right-fringe 16)))
-
-(use-package yasnippet
-  :straight t
-  :config
-  (yas-global-mode))
-
-(use-package yasnippet-snippets :straight t :after 'yasnippet)
-
-(use-package consult-yasnippet :straight t :after 'yasnippet)
-
-(use-package jinx
-  :straight t
-  :config
-  (global-jinx-mode))
-
-(use-package gcmh
-  :straight t
-  :demand
-  :config
-  (gcmh-mode 1))
-
-(use-package ws-butler
-  :straight t
-  :hook ((prog-mode . ws-butler-mode)))
-
-(use-package apheleia
-  :straight t
-  :config
-  (apheleia-global-mode +1))
-
-(use-package copy-as-format
-  :straight t)
 
 (use-package markdown-xwidget
   :after markdown-mode
@@ -414,37 +413,18 @@
              :repo "cfclrk/markdown-xwidget"
              :files (:defaults "resources")))
 
-(use-package abridge-diff
-  :after magit ;; optional, if you'd like to use with magit
-  :init (abridge-diff-mode 1))
+;;; Git/Version Control Tooling
+(use-package magit :straight t)
+(use-package git-timemachine :straight t)
+(use-package abridge-diff :after magit :init (abridge-diff-mode 1))
 
-(use-package sideline-blame :straight t)
-
-(use-package sideline-lsp
-  :straight t)
-
-(use-package sideline
+(use-package magit-delta
   :straight t
-  :init
-  (setq sideline-backends-right '((sideline-blame . down)))
-  :config
-  (global-sideline-mode 1))
+  :hook ((magit-mode . magit-delta-mode)))
 
-(use-package editorconfig
+(use-package git-modes
   :straight t
-  :config
-  (setopt editorconfig-trim-whitespaces-mode 'ws-butler-mode)
-  (editorconfig-mode 1))
-
-(use-package project
-  :config
-  (advice-add 'project-switch-project
-              :before
-              #'kdz/project-open-show-dashboard)
-  (advice-add 'project-switch-project
-              :after
-              #'kdz/project-kill-dashboard))
-
+  :mode (("/.dockerignore\\'" . gitignore-mode )))
 
 ;; TODO This needs tree-sitter to work
 ;; TODO This pulls from quelpa; how do I set that up with straight?
@@ -452,8 +432,3 @@
 ;;   :straight t
 ;;   :config
 ;;   (setq turbo-log-msg-format-template "\"KDZ-LOG: %s\""))
-
-(use-package anzu
-  :straight t
-  :config
-  (global-anzu-mode +1))
