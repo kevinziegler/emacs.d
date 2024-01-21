40a1a2dca4bc0dbb7b1a9f2bd4cd57b802f97b32
Kevin Ziegler <kevinziegler@Balder.asgard>
2024-01-21 13:18:20 -0600

    Split out `org-agenda` configuration

--- packages.d/org.el
+++ packages.d/org.el
@@ -8,16 +8,8 @@
           ;; Org styling, hide markup etc.
           org-hide-emphasis-markers t
           org-pretty-entities t
-          org-ellipsis "…"
-          ;; Agenda styling
-          org-agenda-tags-column 0
-          org-agenda-block-separator ?─
-          org-agenda-time-grid
-          '((daily today require-timed)
-            (800 1000 1200 1400 1600 1800 2000)
-            " ┄┄┄┄┄ " "┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄")
-          org-agenda-current-time-string
-          "⭠ now ─────────────────────────────────────────────────")
+          org-ellipsis " …"
+          org-cycle-separator-lines 0)
 
   (defun kdz/org-heading-fixup-new-line ()
     "Ensure an empty line between non-empty org-mode headings"
@@ -48,7 +40,30 @@
      (python . t)
      (sql . t)
      (sqlite . t)
-     (emacs-lisp .t))))
+     (emacs-lisp .t)))
+
+  ;; (advice-add 'org-babel-variable-assignments:plantuml
+  ;;             :override #'kdz/org-babel-variable-assignments:plantuml)
+  ;; (advice-add 'org-babel-plantuml-make-body
+  ;;             :override #'kdz/org-babel-plantuml-make-body)
+  ;; (advice-add 'org-mks
+  ;;             :override #'org-mks-pretty)
+  ;; (advice-add 'org-capture-select-template
+  ;;             :override #'org-capture-select-template-prettier)
+
+  )
+
+(use-package org-agenda
+  :straight nil
+  :after org
+  :config
+  (setopt org-agenda-tags-column 0
+          org-agenda-block-separator ?─
+          org-agenda-time-grid '((daily today require-timed)
+                                 (800 1000 1200 1400 1600 1800 2000)
+                                 " ┄┄┄┄┄ " "┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄")
+          org-agenda-current-time-string
+          "⭠ now ─────────────────────────────────────────────────"))
 
 (use-package org-modern
   :straight t
@@ -64,14 +79,14 @@
 
   (add-hook 'org-mode-hook 'org-appear-mode)
   (add-hook 'org-mode-hook (lambda ()
-                           (add-hook 'evil-insert-state-entry-hook
-                                     #'org-appear-manual-start
-                                     nil
-                                     t)
-                           (add-hook 'evil-insert-state-exit-hook
-                                     #'org-appear-manual-stop
-                                     nil
-                                     t))))
+                             (add-hook 'evil-insert-state-entry-hook
+                                       #'org-appear-manual-start
+                                       nil
+                                       t)
+                             (add-hook 'evil-insert-state-exit-hook
+                                       #'org-appear-manual-stop
+                                       nil
+                                       t))))
 
 (use-package org-autolist
   :straight t
@@ -79,20 +94,21 @@
   :hook ((org-mode-hook . org-autolist-mode)))
 
 ;; TODO Load this via org-bable-do-load-languages
-(use-package ob-http
-  :straight t
-  :after org)
+(use-package ob-http :straight t :after org)
 
 ;; TODO Set up keybindings
-(use-package org-mac-link :straight t)
+(use-package org-mac-link :straight t
+  :config
+  ;; (advice-add 'org-mac-link-firefox-insert-frontmost-url
+  ;;             :around #'kdz/org-mac-link-advise-evil)
+  ;; (advice-add 'org-mac-link-finder-insert-selected
+  ;;             :around #'kdz/org-mac-link-advise-evil)
+  )
 ;; (use-package 'org-re-reveal :straight t)
 
 ;; TODO Figure out how to get the tree view to show up for this package
-;;(use-package org-sidebar :straight t)
+;; (use-package org-sidebar :straight t)
 
-(use-package org-evil
-  :straight t
-  :after '(evil org))
+(use-package org-evil :straight t :after '(evil org))
 
-;; (use-package ob-http :straight t)
 ;; (use-package ox-pandoc :straight t)
