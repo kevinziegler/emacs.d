13e762567e26ccdf2b7d9a2f77e50d098843497e
Kevin Ziegler <kevinziegler@Balder.asgard>
2024-01-25 10:39:00 -0600

    Add `difftastic` package

--- packages.d/utilities.el
+++ packages.d/utilities.el
@@ -582,6 +582,17 @@ actions that would update colors in emacs (such as changing themes)"
   :straight t
   :hook ((magit-mode . magit-delta-mode)))
 
+(use-package difftastic
+  :straight t
+  :bind (:map magit-blame-read-only-mode-map
+              ("D" . difftastic-magit-show)
+              ("S" . difftastic-magit-show))
+  :config
+  (eval-after-load 'magit-diff
+    '(transient-append-suffix 'magit-diff '(-1 -1)
+       [("D" "Difftastic diff (dwim)" difftastic-magit-diff)
+        ("S" "Difftastic show" difftastic-magit-show)])))
+
 (use-package git-modes
   :straight t
   :mode (("/.dockerignore\\'" . gitignore-mode )))
