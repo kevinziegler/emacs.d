bc46c0eabd52b8533662064baab88baeb10ee8ff
Kevin Ziegler <kevinziegler@Balder.asgard>
2024-02-11 11:03:41 -0600

    Clean up transient path helpers

--- lisp/packages.d/ui/base.el
+++ lisp/packages.d/ui/base.el
@@ -46,12 +46,11 @@
 (use-package transient
   :straight t
   :config
-  (setq transient-history-file (kdz/user-directory ".local"
-                                                   "transient"
-                                                   "history.el")
-        transient-levels-file  (kdz/user-directory ".local"
-                                                   "transient"
-                                                   "levels.el")
-        transient-values-file  (kdz/user-directory ".local"
-                                                   "transient"
-                                                   "values.el")))
+  (defun kdz/transient-path (file)
+    (kdz/user-directory ".local" "transient" file))
+
+  (setq transient-history-file (kdz/transient-path "history.el")
+        transient-levels-file  (kdz/transient-path "levels.el")
+        transient-values-file  (kdz/transient-path "values.el")))
+
+(use-package avy :straight t)
