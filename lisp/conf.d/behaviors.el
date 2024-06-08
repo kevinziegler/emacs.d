fb0d14e327ca6a5fa94bafaad283c6d70bfa0de5
Kevin Ziegler <kevinziegler@Balder.asgard>
2024-06-08 11:50:02 -0500

    Use spaces instead of tabs in `behaviors.el`

--- lisp/conf.d/behaviors.el
+++ lisp/conf.d/behaviors.el
@@ -1,51 +1,53 @@
 (setq switch-to-buffer-obey-display-actions t)
 
 (add-to-list 'display-buffer-alist
-	     '("\\*ielm\\*"
-	       display-buffer-in-side-window
-	       (side . bottom)
-	       (slot . 99)
-	       (dedicated . t)))
+             '("\\*ielm\\*"
+               display-buffer-in-side-window
+               (side . bottom)
+               (slot . 99)
+               (dedicated . t)))
 
 (add-to-list 'display-buffer-alist
-	     '("\\*helpful [a-z]+: .+\\*"
-	       display-buffer-in-side-window
-	       (side . right)
-	       (dedicated . t)))
+             '("\\*helpful [a-z]+: .+\\*"
+               display-buffer-in-side-window
+               (side . right)
+               (dedicated . t)))
 
 (add-to-list 'display-buffer-alist
-	     '("\\*Ibuffer\\*"
-	       display-buffer-in-tab
-	       (tab-name . "Buffers")))
 
 (add-to-list 'display-buffer-alist
-	     '("\\*Messages\\*"
-	       display-buffer-in-tab
-	       (tab-name . "System")))
+             '("\\*Ibuffer\\*"
+               display-buffer-in-tab
+               (tab-name . "Buffers")))
 
 (add-to-list 'display-buffer-alist
-	     '("\\*Warnings\\*"
-	       display-buffer-in-tab
-	       (tab-name . "System")))
+             '("\\*Messages\\*"
+               display-buffer-in-tab
+               (tab-name . "System")))
 
 (add-to-list 'display-buffer-alist
-	     '("\\*lsp-install: .+\\*"
-	       display-buffer-in-tab
-	       (tab-name . "System")))
+             '("\\*Warnings\\*"
+               display-buffer-in-tab
+               (tab-name . "System")))
 
 (add-to-list 'display-buffer-alist
-	     '("\\*straight-process\\*"
-	       display-buffer-in-tab
-	       (tab-name . "Packages")))
+             '("\\*lsp-install: .+\\*"
+               display-buffer-in-tab
+               (tab-name . "System")))
 
 (add-to-list 'display-buffer-alist
-	     '("\\*Packages\\*"
-	       display-buffer-in-tab
-	       (tab-name . "Packages")))
+             '("\\*straight-process\\*"
+               display-buffer-in-tab
+               (tab-name . "Packages")))
 
 (add-to-list 'display-buffer-alist
-	     '("\\*dashboard\\*"
-	       display-buffer-in-tab
-	       (tab-name . "Home")))
+             '("\\*Packages\\*"
+               display-buffer-in-tab
+               (tab-name . "Packages")))
+
+(add-to-list 'display-buffer-alist
+             '("\\*dashboard\\*"
+               display-buffer-in-tab
+               (tab-name . "Home")))
 
 (provide 'conf.d/behaviors)
