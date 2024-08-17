a2cb35d1b077a88e5a3dc907a37103c3db4a1e31
Kevin Ziegler <kevinziegler@Balder.local>
2024-08-17 13:21:36 -0500

    Add back posframe configuration

--- lisp/packages.d/ui/posframe.el
+++ lisp/packages.d/ui/posframe.el
@@ -24,39 +24,6 @@
                       posframe-height
                       (* offset-percent parent-frame-height)))))))
 
-(use-package which-key-posframe
-  :straight t
-  :config
-  ;; Stand-in until the following issue is merged:
-  ;; https://github.com/yanghaoxie/which-key-posframe/pull/21
-  ;;
-  ;; Note that my version *also* tweaks the width parameter
-  (defun kdz/fixup--which-key-posframe--show-buffer (act-popup-dim)
-    "Override which-key-posframe parameters to ensure content is visible"
-    (when (posframe-workable-p)
-      (save-window-excursion
-        (posframe-show
-         which-key--buffer
-         :font which-key-posframe-font
-         :position (point)
-         :poshandler which-key-posframe-poshandler
-         :background-color (face-attribute 'which-key-posframe :background nil t)
-         :foreground-color (face-attribute 'which-key-posframe :foreground nil t)
-         :height (ceiling (* 1.25 (car act-popup-dim)))
-         :width (ceiling (* 1.1 (cdr act-popup-dim)))
-         :internal-border-width which-key-posframe-border-width
-         :internal-border-color (face-attribute 'which-key-posframe-border
-                                                :background nil
-                                                t)
-         :override-parameters which-key-posframe-parameters))))
-
-  (advice-add #'which-key-posframe--show-buffer
-              :override
-              #'kdz/fixup--which-key-posframe--show-buffer)
-
-  (setq which-key-posframe-poshandler 'kdz/posframe-offset-bottom)
-  (which-key-posframe-mode 1))
-
 (use-package vertico-posframe
   :straight t
   :after (posframe vertico)
