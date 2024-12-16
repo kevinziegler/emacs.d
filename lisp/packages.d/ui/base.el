84d6bf663dc8dffcd898d416ed3a1faf057b88f9
Kevin Ziegler <keziegler@paypal.com>
2024-12-16 09:28:26 -0600

    Make custom nerd-icons functions more dynamic

--- lisp/packages.d/ui/base.el
+++ lisp/packages.d/ui/base.el
@@ -3,11 +3,48 @@
 (use-package nerd-icons
   :straight t
   :config
-  (defun kdz/propertize-nerd-icon (name face-function)
-    (propertize (nerd-icons-mdicon name)
-                'face `(:family ,(funcall face-function) :height 1.2)
-                'font-lock-face `(:family ,(funcall face-function) :height 1.2)
-                'display '(raise 0)) ))
+
+  (defvar kdz-nerd-icons-function-map
+    '(("nf-cod"     . nerd-icons-codicon)
+      ("nf-dev"     . nerd-icons-devicon)
+      ("nf-fa"      . nerd-icons-faicon)
+      ("nf-iec"     . nerd-icons-ipsicon)
+      ("nf-md"      . nerd-icons-mdicon)
+      ("nf-oct"     . nerd-icons-octicon)
+      ("nf-pom"     . nerd-icons-pomicon)
+      ("nf-seti"    . nerd-icons-sucicon)
+      ("nf-custom"  . nerd-icons-sucicon)
+      ("nf-weather" . nerd-icons-wicon))
+    "Mapping of nf-<iconset> names to their nerd-icons-* function ")
+
+  (defvar kdz-nerd-icons-family-function-map
+    '(("nf-cod"     . nerd-icons-codicon-family)
+      ("nf-dev"     . nerd-icons-devicon-family)
+      ("nf-fa"      . nerd-icons-faicon-family)
+      ("nf-iec"     . nerd-icons-ipsicon-family)
+      ("nf-md"      . nerd-icons-mdicon-family)
+      ("nf-oct"     . nerd-icons-octicon-family)
+      ("nf-pom"     . nerd-icons-pomicon-family)
+      ("nf-seti"    . nerd-icons-sucicon-family)
+      ("nf-custom"  . nerd-icons-sucicon-family)
+      ("nf-weather" . nerd-icons-wicon-family))
+    "Mapping of nf-<iconset> names to their nerd-icons-* function ")
+
+  (defun kdz/nerd-icons-dwim (name)
+    "Get a nerd-icons icon with name, regardless of icon set"
+    (funcall (cdr (seq-find (lambda (mapping)
+                              (s-starts-with? (car mapping) name))
+                            kdz-nerd-icons-function-map))
+             name))
+
+  (defun kdz/propertize-nerd-icon (name &optional face-function)
+    (let ((face-family (funcall (cdr (seq-find (lambda (mapping)
+                                                 (s-starts-with? (car mapping) name))
+                                               kdz-nerd-icons-family-function-map)) )))
+      (propertize (kdz/nerd-icons-dwim name)
+                  'face `(:family ,face-family :height 1.2)
+                  'font-lock-face `(:family ,face-family :height 1.2)
+                  'display '(raise 0)) ) ))
 
 (use-package pretty-hydra :straight t)
 
