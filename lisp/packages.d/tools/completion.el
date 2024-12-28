24016648e36e12329c16239abe2a04ba673de343
Kevin Ziegler <kevinziegler@Balder.asgard>
2024-12-28 11:33:18 -0600

    Restrict `embark` bindings to `vertico-map`

--- lisp/packages.d/tools/completion.el
+++ lisp/packages.d/tools/completion.el
@@ -53,11 +53,13 @@
 
 (use-package embark
   :straight t
-  :bind
-  (("C-." . embark-act)             ;; pick some comfortable binding
-   ("C-;" . embark-dwim)            ;; good alternative: M-.
-   ("C-h B" . embark-bindings)      ;; alternative for `describe-bindings'
-   ("s-<return>" . embark-collect)) ;; 90% of what I want to do is persist results
+  :general
+  (general-def
+    :keymaps 'vertico-map
+    ("C-." . embark-act)             ;; pick some comfortable binding
+    ("C-;" . embark-dwim)            ;; good alternative: M-.
+    ("C-h B" . embark-bindings)      ;; alternative for `describe-bindings'
+    ("s-<return>" . embark-collect)) ;; 90% of what I want to do is persist results
 
   :init
   ;; Optionally replace the key help with a completing-read interface
