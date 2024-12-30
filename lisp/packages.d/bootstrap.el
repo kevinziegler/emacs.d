0d456814ece259c3a262ccfc71e9f45faf90f5d3
Kevin Ziegler <kevinziegler@Balder.asgard>
2024-12-30 10:55:26 -0600

    Clean up last bits of `straight.el` config

--- lisp/packages.d/bootstrap.el
+++ lisp/packages.d/bootstrap.el
@@ -1,24 +1,3 @@
-;; (defvar bootstrap-version)
-;; (setq straight-base-dir (kdz/user-directory ".local"))
-
-;; (let ((bootstrap-file (kdz/user-directory ".local"
-;;                                           "straight"
-;;                                           "repos"
-;;                                           "straight.el"
-;;                                           "bootstrap.el"))
-;;       (bootstrap-version 6))
-;;   (unless (file-exists-p bootstrap-file)
-;;     (with-current-buffer
-;;         (url-retrieve-synchronously
-;;          "https://raw.githubusercontent.com/radian-software/straight.el/develop/install.el"
-;;          'silent 'inhibit-cookies)
-;;       (goto-char (point-max))
-;;       (eval-print-last-sexp)))
-;;   (load bootstrap-file nil 'nomessage))
-
-;; (straight-use-package 'use-package)
-;; (setq straight-use-package-by-default t)
-
 (defvar elpaca-installer-version 0.8)
 (defvar elpaca-directory (kdz/user-directory ".local" "elpaca"))
 (defvar elpaca-builds-directory (expand-file-name "builds/" elpaca-directory))
