6234d4f3bfec8ce23468034b5fd9ae89cf318cba
kevinziegler <keziegler@paypal.com>
2025-02-12 12:30:12 -0600

    Remove workaround for `corfu` bug
    This issue has been fixed upstream and the custom advice is no longer required.

--- lisp/packages.d/tools/completion.el
+++ lisp/packages.d/tools/completion.el
@@ -10,16 +10,7 @@
   :custom
   (corfu-auto t)
   :config
-  (global-corfu-mode)
-
-  ;; https://github.com/emacs-evil/evil-collection/issues/766
-  (advice-remove 'corfu--setup 'evil-normalize-keymaps)
-  (advice-remove 'corfu--teardown 'evil-normalize-keymaps)
-
-  (advice-add 'corfu--setup
-              :after (lambda (&rest r) (evil-normalize-keymaps)))
-  (advice-add 'corfu--teardown
-              :after (lambda (&rest r) (evil-normalize-keymaps))))
+  (global-corfu-mode))
 
 (use-package nerd-icons-corfu
   :after corfu
