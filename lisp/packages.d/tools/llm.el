cb0be6ec3d08441e326fa295f57d6efe053a7f88
kevinziegler <keziegler@paypal.com>
2025-08-15 09:39:51 -0500

    Add 'aidermacs' package

--- lisp/packages.d/tools/llm.el
+++ lisp/packages.d/tools/llm.el
@@ -1,4 +1,9 @@
 (use-package copilot)
 (use-package copilot-chat)
 
+(use-package aidermacs
+  :custom
+  (aidermacs-default-chage-mode 'architect)
+  (aidermacs-default-model "sonnet"))
+
 (provide 'packages.d/tools/llm)
